349507c57167fb41168de81cf1fb030c02712597
Ludvig <ludvigvonreis@gmail.com>
2026-09-08 23:15:12 +0200

    nvim: Fixed some problems with transparency and lsp stuff

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,21 +1,30 @@
 -- Telescope bindings
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>sk", builtin.keymaps, {
-    desc = "[S]earch [K]eymaps"
+	desc = "[S]earch [K]eymaps",
 })
 vim.keymap.set("n", "<leader>f", builtin.find_files, {
-    desc = "Search [F]iles"
+	desc = "Search [F]iles",
 })
 vim.keymap.set("n", "<leader>g", builtin.current_buffer_fuzzy_find, {
-    desc = "Search by [G]rep"
+	desc = "Search by [G]rep",
 })
 vim.keymap.set("n", "<leader><leader>", builtin.buffers, {
-    desc = "Find open buffers"
+	desc = "Find open buffers",
 })
 vim.keymap.set("n", "<leader>sn", function()
-    builtin.find_files({
-        cwd = vim.fn.stdpath("config")
-    })
+	builtin.find_files({
+		cwd = vim.fn.stdpath("config"),
+	})
 end, {
-    desc = "[S]earch [N]eovim files"
+	desc = "[S]earch [N]eovim files",
 })
+
+local function run_in_terminal(cmd)
+	local cwd = vim.fn.expand("%:p:h")
+	vim.fn.jobstart({ "kitty", "--directory", cwd, "-e", "sh", "-c", cmd .. "; exec zsh" }, { detach = true })
+end
+
+vim.api.nvim_create_user_command("Term", function(opts)
+	run_in_terminal(opts.args)
+end, { nargs = 1, desc = "Run a shell command in a new OS terminal" })
--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,16 @@
 return { -- lazy.nvim
-{
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- add any options here
-    },
-    dependencies = {"MunifTanjim/nui.nvim", "rcarriga/nvim-notify"}
-}}
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+	},
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			background_colour = "#232833", -- match your Normal bg
+		},
+	},
+}
