717b9ce68acce2b17831e7c87d6964612f4a5ab7
Ludvig <ludvigvonreis@gmail.com>
2026-09-17 16:29:56 +0200

    Merge i think

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,7 +1,14 @@
 bg = "rgb(131313)"
 fg = "rgb(e2e2e2)"
+<<<<<<< Updated upstream
 accent = "rgb(ffb5a1)"
 muted = "rgb(474747)"
 
 color_active_border = "rgb(ffb5a1)"
+=======
+accent = "rgb(a0c9ff)"
+muted = "rgb(474747)"
+
+color_active_border = "rgb(a0c9ff)"
+>>>>>>> Stashed changes
 color_inactive_border = "rgb(474747)"
